22cd0c8d3fd8aae89c9a896e3a2c09e9a31b08e2
Shaquu <tadev3@gmail.com>
2018-05-19 21:51:00 +0200

    Trying to fix resourc walker
    Signed-off-by: Shaquu <tadev3@gmail.com>

--- src/io/github/shaquu/memory/game/utils/CardImageManager.java
+++ src/io/github/shaquu/memory/game/utils/CardImageManager.java
@@ -11,8 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,28 +24,36 @@ public class CardImageManager {
     public CardImageManager() throws URISyntaxException, IOException {
         imageNameList = new LinkedHashMap<>();
 
-        URI imageFolder = getClass().getResource("/image/card/").toURI();
-        if(new File(imageFolder).isDirectory()){
-            Files.walk(Paths.get(imageFolder))
-                    .filter(Files::isRegularFile)
-                    .forEach(path -> {
-                        File file = path.toFile();
-                        String fileName = file.getName();
-
-                        Pattern p = Pattern.compile("\\d{3}-(.*?)-card.png");
-                        Matcher m = p.matcher(fileName);
-
-                        if (m.find()) {
-                            String[] data = m.group(1).split("-");
-                            StringBuilder name = new StringBuilder();
-                            for(String d : data){
-                                name.append(Character.toUpperCase(d.charAt(0))).append(d.substring(1)).append(" ");
-                            }
-                            name = new StringBuilder(name.substring(0, name.length() - 1));
-                            imageNameList.put(name.toString(), fileName);
-                        }
-                    });
+        URI uri = CardImageManager.class.getResource("/image/card/").toURI();
+        Path myPath;
+        if (uri.getScheme().equals("jar")) {
+            FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
+            myPath = fileSystem.getPath("/resources");
+        } else {
+            myPath = Paths.get(uri);
         }
+
+        Files.walk(myPath, 1)
+                .filter(Files::isRegularFile)
+                .forEach(path -> {
+                    File file = path.toFile();
+                    String fileName = file.getName();
+
+                    Pattern p = Pattern.compile("\\d{3}-(.*?)-card.png");
+                    Matcher m = p.matcher(fileName);
+
+                    if (m.find()) {
+                        String[] data = m.group(1).split("-");
+                        StringBuilder name = new StringBuilder();
+                        for (String d : data) {
+                            name.append(Character.toUpperCase(d.charAt(0))).append(d.substring(1)).append(" ");
+                        }
+                        name = new StringBuilder(name.substring(0, name.length() - 1));
+                        imageNameList.put(name.toString(), fileName);
+                    }
+                });
+
+        GameLogger.log("Loaded images : " + imageNameList.size());
     }
 
     String getNameAt(int index) {
